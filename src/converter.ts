cb8355ea2cbe875d5619a4895bfa8d41b7fc0839
Shuvojit <s15sarkar@yahoo.com>
2024-04-29 10:16:47 +0530

    added yaml package; initial logic complete

--- src/converter.ts
+++ src/converter.ts
@@ -1,29 +1,69 @@
+import {parse, stringify} from 'yaml';
+
 function isEncoded(source: string): boolean {
   return false;
 }
 
 function baseDecode(source: string): string {
-  return "";
+  return atob(source);
 }
 
 function baseEncode(source: string): string {
-  return "";
+  return btoa(source);
 }
 
 export enum DocumentSummary {
   PLAINTEXT,
   ENCODED,
-  MIXED 
+  MIXED,
+  UNKNOWN
 }
 
 export function getDocumentSummary(source: string): DocumentSummary {
+  const doc = parse(source);
+  if (!('data' in doc)) {
+    return DocumentSummary.UNKNOWN;
+  }
+
+  let total = 0, encoded = 0;
+  for(const key in doc.data) {
+    const value = doc.data[key];
+    if (isEncoded(value)) encoded++;
+    total++;
+  }
+
+  if (encoded === 0) return DocumentSummary.PLAINTEXT;
+  if (total === encoded) return DocumentSummary.ENCODED;
   return DocumentSummary.MIXED;
 }
 
 export function encodeAll(source: string): string {
-  return source;
+  const doc = parse(source);
+  if (!('data' in doc)) {
+    return source;
+  }
+
+  for(const key in doc.data) {
+    const value = doc.data[key];
+    if (!isEncoded(value)) {
+      doc.data[key] = baseEncode(value);
+    }
+  }
+  return stringify(source);
 }
 
 export function decodeAll(source: string): string {
-  return source;
+  const doc = parse(source);
+  if (!('data' in doc)) {
+    return source;
+  }
+
+  for(const key in doc.data) {
+    const value = doc.data[key];
+    if (isEncoded(value)) {
+      doc.data[key] = baseDecode(value);
+    }
+  }
+  return stringify(source);
+
 }
